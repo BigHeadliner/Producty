3724029da0ac3e570fd5c4496643d3d6773220e9
BigHeadliner <bigheadliner@gmail.com>
2022-05-27 20:20:01 +0300

    header adaptive ready

--- js/main.js
+++ js/main.js
@@ -5,5 +5,11 @@ $(function(){
     slidesToScroll: 3,
     arrows: false, 
     dots:true,
-   });
-});
+   });  
+    
+   $('.menu__btn').on('click', function(){ 
+       $('.menu__list').toggleClass('menu__list--active')
+   })
+
+}); 
+  
